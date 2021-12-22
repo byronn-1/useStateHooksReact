80777b6ae175b66a564ec84a604b9617baf8b5f2
Byron Lee <byronn.lee@gmail.com>
2021-12-22 05:11:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/accordion';
 
-function App() {
+const items = [
+  {
+    title: 'What is react?',
+    content: 'React is a front end Js framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a fave js library amoug engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  },
+]
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <div>
+      <Accordion items={items} />
+    </div >
+  )
+};
