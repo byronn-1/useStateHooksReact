b9e31f65c8edd773a54847dd2b3c7669e351bf22
Byron Lee <byronn.lee@gmail.com>
2021-12-24 14:36:54 +0000

    created dropdown jsx

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
-const dropdown = () => {
-  return <h1>Dropdown</h1>
+const dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value}
+        className='item'>
+        {option.label}
+      </div>
+    )
+  })
+  return (
+    <div className='ui form'>
+      <div className='field'>
+        <label className='label'>Select a color</label>
+        <div className='ui selection dropdown visable active '>
+          <i className='dropdown icon'></i>
+          <div className='text'>Select Colour</div>
+          <div className='menu visable transition'>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
